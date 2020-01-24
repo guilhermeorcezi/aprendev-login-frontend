557a8f3464f2306079326c788678fab15ba84fac
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-01-23 22:49:28 -0300

    frontend 1.0

---
+++ src/pages/Login/login.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import './index.css';
+import api from '../../services/api';
+import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { css } from 'glamor';
+
+export default function Login( {history} ){
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    
+    async function handleSubmit(e){
+        e.preventDefault();
+
+        const response = await api.post('/login',{
+            user,
+            password
+        });
+        
+        const userExist = response.data.user_exist;
+        const isValid = response.data.logged_in;
+
+        if(isValid){
+            history.push('/main');
+        }else{
+            if(userExist){
+                return handleError();
+            }
+            if(!userExist){
+                return handleUserError();
+            }
+        } 
+    }
+
+    function handleError(){
+        toast.error('Senha incorreta', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,className: css({
+                background: '#E71E28',
+                color:'#FFF',
+              }),
+        });
+    }
+
+    function handleUserError(){
+        toast.error('Usuário não existe', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,className: css({
+                background: '#E71E28',
+                color:'#FFF',
+              }),
+        });
+    }
+
+    return (
+        <main>
+            <ToastContainer />
+            <div className="login-box">
+                <strong>ENTRAR</strong>
+                    <div className="form-login">
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="user">Usuário</label>
+                            <input 
+                            name="user" 
+                            id="user" 
+                            required
+                            value={user} 
+                            onChange={event => setUser(event.target.value)}
+                            /> 
+
+                            <label htmlFor="pwd">Senha</label>
+                            <input 
+                            type="password" 
+                            name="password" 
+                            id="paspword"
+                            required
+                            value={password}
+                            onChange={event => setPassword(event.target.value)}
+                            />
+
+                            <button type="submit">Efetuar Login</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    );
+}
---
+++ src/pages/Main/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+
+export default function Login(){
+    return (
+        <main>
+            <div className="welcome-box">
+                <h1>Welcome </h1>
+            </div>
+        </main>
+    );
+}
